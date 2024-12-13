ef1c897404bb1a740a55c45308d4ca8073774261
Arkaitz Amostegui <ikdge@plaiaundi.net>
2024-12-13 18:10:07 +0100

    Iniciando repositorio

---
+++ persona.java
@@ -0,0 +1,10 @@
+public class persona {
+    
+	private String nombre;
+	private int edad;
+	private String DNI;
+	private char sexo;
+	private double peso;
+	private double altura;
+	
+}
---
+++ src/PersonaMain.java
@@ -0,0 +1,4 @@
+public class PersonaMain {
+    public static void main(String[] args) throws Exception {
+    }
+}
