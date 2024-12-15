797ec5c49ce9e7d640fcc6bf025cd7c7f377ccd2
Arkaitz Amostegui <ikdge@plaiaundi.net>
2024-12-15 12:52:16 +0100

    Solucionado conflicto

--- persona.java
+++ persona.java
@@ -7,13 +7,14 @@ public class persona {
 	private double peso;
 	private double altura;
 	
-}
+
 //Iniciar atributos:
 public persona () {
 	this.nombre=""; //Está vacío porque no sabemos el nombre
 	this.edad=0; //empezamos desde la edad 0
-	this.dni=""; //No sabemos el DNI, empieza vacío
-	this.sexo= GeneroPorDefecto; // elegir entre M y H, podemos iniciarlo con'H, necesario constante.**falta poner la constante!!
+	this.DNI=""; //No sabemos el DNI, empieza vacío
+	this.sexo= 'M'; // elegir entre M y H, podemos iniciarlo con'H, necesario constante.**falta poner la constante!!
 	this.peso=0; //iniciamos en 0
 	this.altura=0; //iniciamos en 0
+}
 }
